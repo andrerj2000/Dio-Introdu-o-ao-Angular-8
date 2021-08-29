cbb4c34292ff8b3213130bbc16a8485b7d2f8479
andrerj2000 <andrerj2000@gmail.com>
2021-08-29 14:39:45 -0300

    Implementation Angular 8 start

---
+++ course-manager/src/app/course/course-list.components.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-course-lits',
+  templateUrl: './course-list.components.html'
+})
+export class CourseListComponent{
+
+}
